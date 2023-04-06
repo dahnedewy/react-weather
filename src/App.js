eb35c66a123d8398ba8793676298d5dd5e5d468b
dahnedewy <121363790+dahnedewy@users.noreply.github.com>
2023-04-06 17:03:41 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <a href="https://github.com/dahnedewy/react-weather"></a>
+    </div>
+  );
 }
 
 export default App;
